a2b142e6630417eeed89aa744e1ddd8a2b25d488
Tim T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-08-09 16:25:39 +0000

    added apptheme

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AppRoutes from "@routes/index";
+import { ThemeProvider } from "@fluentui/react";
+import appTheme from "@components/ui/theme.ui";
 
 const router = createBrowserRouter(AppRoutes)
 function App() {
 
   return (
-    <RouterProvider router={router} />
+    <ThemeProvider theme={appTheme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   );
 }
 
---
+++ client/src/components/ui/theme.ui.jsx
@@ -0,0 +1,27 @@
+const appTheme = {
+  palette: {
+    themePrimary: "#ff5460",
+    themeLighterAlt: "#0a0304",
+    themeLighter: "#290d0f",
+    themeLight: "#4d191d",
+    themeTertiary: "#993239",
+    themeSecondary: "#e04a54",
+    themeDarkAlt: "#ff656f",
+    themeDark: "#ff7d86",
+    themeDarker: "#ff9fa6",
+    neutralLighterAlt: "#000000",
+    neutralLighter: "#000000",
+    neutralLight: "#000000",
+    neutralQuaternaryAlt: "#000000",
+    neutralQuaternary: "#000000",
+    neutralTertiaryAlt: "#000000",
+    neutralTertiary: "#c8c8c8",
+    neutralSecondary: "#d0d0d0",
+    neutralPrimaryAlt: "#dadada",
+    neutralPrimary: "#ffffff",
+    neutralDark: "#f4f4f4",
+    black: "#f8f8f8",
+    white: "#000000",
+  },
+};
+export default appTheme;
