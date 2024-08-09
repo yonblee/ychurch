6d6431f7d9dfb1267935f52634a588b45cf9cd77
Tim T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-08-09 12:42:36 +0000

    added vite alias files

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      {
+        find: "@services",
+        replacement: path.resolve(__dirname, "src/services"),
+      },
+      {
+        find: "@components",
+        replacement: path.resolve(__dirname, "src/components"),
+      },
+      { find: "@pages", replacement: path.resolve(__dirname, "src/pages") },
+      { find: "@redux", replacement: path.resolve(__dirname, "src/redux") },
+      { find: "@assets", replacement: path.resolve(__dirname, "src/assets") },
+      { find: "@utils", replacement: path.resolve(__dirname, "src/utils") }, 
+      { find: "@modules", replacement: path.resolve(__dirname, "src/modules") }, 
+    ],
+  },
+  server: {
+    port: 4500,
+    open: true,
+  },
+});
