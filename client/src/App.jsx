1bf046f5ce598c3583822813e1172faed6aacf39
Tim T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-08-10 15:20:35 +0000

    add griffel css-in-js package

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AppRoutes from "@routes/index";
 import { ThemeProvider, Toggle } from "@fluentui/react";
 import { lightTheme, darkTheme } from "@components/ui/app.theme";
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 
 const router = createBrowserRouter(AppRoutes);
 function App() {
@@ -15,20 +15,12 @@ function App() {
 
   useEffect(() => {
     let mode = Boolean(window.localStorage.getItem("mode"));
-    setMode(mode)
+    setMode(mode);
   }, []);
-
-
 
   return (
     <ThemeProvider theme={mode ? lightTheme : darkTheme} applyTo="body">
       <RouterProvider router={router} />
-      <Toggle
-        label="Change themes"
-        onText="Dark Mode"
-        offText="Light Mode"
-        onChange={handleTheme}
-      />
     </ThemeProvider>
   );
 }
