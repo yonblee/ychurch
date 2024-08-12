eda566fba07cf7baaf6b46ee19793b864d00176f
Tim T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-08-12 10:58:12 +0000

    added reset routes

--- client/src/routes/auth/auth.routes.jsx
+++ client/src/routes/auth/auth.routes.jsx
@@ -1,11 +1,13 @@
 import RootElem from "@routes/root.elem";
 import FindRoutes from "./find.routes";
+import ResetRoutes from "./reset.routes";
 
 const AuthRoutes = {
   path: "/auth/:paths",
   element: <RootElem />,
   children: [
-    <FindRoutes />
+    <FindRoutes />,
+    <ResetRoutes/>
   ]
 };
 
---
+++ client/src/routes/auth/reset.routes.jsx
@@ -0,0 +1,8 @@
+import AuthReset from "@pages/auth/auth.reset";
+
+const ResetRoutes = {
+  path: "reset",
+  element: <AuthReset />,
+};
+
+export default ResetRoutes;
