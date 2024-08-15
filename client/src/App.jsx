e90f4506e4010235c9b14883920d443ebd1701ee
Tim T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-08-15 13:43:43 +0000

    learned about fluentUI...

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,17 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { FluentProvider, Switch, ToggleButton, webLightTheme, webDarkTheme} from "@fluentui/react-components";
+import {
+  FluentProvider,
+  webLightTheme,
+  webDarkTheme,
+} from "@fluentui/react-components";
 import AppRoutes from "@routes/app.routes";
 import { useState } from "react";
 
 const router = createBrowserRouter(AppRoutes);
 
 function App() {
-  const [mode, setMode] = useState(true);
-  function handleMode() {
-    setMode(!mode)
-  }
   return (
-    <FluentProvider theme={mode ? webLightTheme : webDarkTheme}>
-      {/* <Switch onClick={handleMode} label="This is a switch"/> */}
+    <FluentProvider theme={webLightTheme}>
       <RouterProvider router={router} />
     </FluentProvider>
   );
--- client/src/routes/app.routes.jsx
+++ client/src/routes/app.routes.jsx
@@ -1,9 +1,13 @@
+import HomePage from "@pages/home/home.page";
 import RootElem from "./root.elem";
 
 const AppRoutes = [
   {
     path: "/",
     element: <RootElem />,
+    children: [
+      {index: true, element: <HomePage />}
+    ]
   },
 ];
 export default AppRoutes;
