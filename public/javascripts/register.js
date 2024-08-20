1ec2f491873f1f59dc4c187da83e5499e60b0fd1
Tim T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-08-20 21:44:45 +0000

    modified register page

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -1,6 +1,12 @@
 var switcher = document.getElementById("switcher");
 var contact = document.getElementById("contact");
 
+window.onload = function onload() {
+  console.log("0880" + Math.floor(Math.random() * 899999 + 100000))
+  // console.log(Math.floor(100000 * Math.random() * 900000))
+}
+
+
 switcher.addEventListener("click", (evt) => {
   let type = contact.type;
   if (type == "text") {
--- routes/register.routes.js
+++ routes/register.routes.js
@@ -1,23 +1,32 @@
 var express = require("express");
 const db = require("../public/javascripts/db");
 var router = express.Router();
-
+var crypto = require("node:crypto");
+var bcrypt = require("bcrypt");
 
 router.get("/", function read(req, res, next) {
   res.render("register", { title: "Register" });
 });
 
 router.post("/", function create(req, res, next) {
-
-  db.run("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT,UUID VARCHAR(50) NOT NULL, fullname VARCHAR(50) NOT NUll, email VARCHAR(50) NOT NULL, password VARCHAR(50) NOT NULL)"
+  db.run(
+    "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, fullname VARCHAR(50) NOT NUll, contact VARCHAR(50) NOT NULL, password VARCHAR(50) NOT NULL,UUID VARCHAR(50) NOT NULL)"
   );
+ let UUID = crypto.randomBytes(8).toString("hex").toUpperCase()
+ let {fullname, email:contact, password} = req.body 
+ const hashed = bcrypt.hashSync(password, 10)
+//  console.log(bcrypt.compareSync("12341234", hashed))
 
   try {
-    db.run("INSERT INTO users VALUES(?,?,?,?,?)", [null,UUID,fullname,email,password], function cb(err){
-
+    db.run("INSERT INTO users VALUES(?,?,?,?,?)", [null,UUID,fullname,contact,hashed], function cb(err){
+      if(err) {
+        res.status(500).json({error:err})
+      } else {
+        console.log("new user created")
+      }
     })
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 });
 
