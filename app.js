ddb1aea3d0632c9c58e3f5998af16b85fa0bb710
Tim T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-08-20 12:29:37 +0000

    added registration page

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var loginRouter = require("./routes/login.routes")
+var registerRouter = require("./routes/register.routes")
 
 var app = express();
 
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // ROUTES handler
 app.use('/', indexRouter);
 app.use('/login', loginRouter);
+app.use('/register', registerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
