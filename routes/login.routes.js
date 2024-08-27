66fa0026449b613e5002d1f274627055e59a5152
Tim T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-08-27 11:40:29 +0000

    added news features

--- routes/login.routes.js
+++ routes/login.routes.js
@@ -1,8 +1,12 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/", function getAuth(req, res, next) {
+router.get("/", function (req, res, next) {
   res.render("login", {title: "Log in"});
+});
+
+router.post("/", function (req, res, next) {
+  let {email,password} = req.body
 });
 
 
--- routes/register.routes.js
+++ routes/register.routes.js
@@ -10,24 +10,20 @@ router.get("/", function read(req, res, next) {
 
 router.post("/", function create(req, res, next) {
   db.run(
-    "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, fullname VARCHAR(50) NOT NUll, contact VARCHAR(50) NOT NULL, password VARCHAR(50) NOT NULL,UUID VARCHAR(50) NOT NULL)"
+    "CREATE TABLE IF NOT EXISTS users (UUID VARCHAR(50) NOT NULL, fullname VARCHAR(50) NOT NUll, contact VARCHAR(50) NOT NULL, password VARCHAR(50) NOT NULL)"
   );
  let UUID = crypto.randomBytes(8).toString("hex").toUpperCase()
- let {fullname, email:contact, password} = req.body 
+ let {fullname, contact, password} = req.body 
  const hashed = bcrypt.hashSync(password, 10)
 //  console.log(bcrypt.compareSync("12341234", hashed))
 
-  try {
-    db.run("INSERT INTO users VALUES(?,?,?,?,?)", [null,UUID,fullname,contact,hashed], function cb(err){
+    db.run("INSERT INTO users VALUES(?,?,?,?)", [UUID,fullname,contact,hashed], function cb(err){
       if(err) {
         res.status(500).json({error:err})
       } else {
         console.log("new user created")
       }
     })
-  } catch (error) {
-    console.error(error);
-  }
 });
 
 module.exports = router;
