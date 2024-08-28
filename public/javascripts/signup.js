1a942004385c3ddd83520924c1743dafb8412830
Tim T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-08-28 10:56:37 +0000

    Added/Modified: > error tag > timer to resend code > verification code input hint > password length/character hint > disable features for buttons > functionalize frequent event in signup.js codebase

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -1,11 +1,11 @@
-var length = document.getElementById("length")
-var chars = document.getElementById("chars")
-var validator = document.getElementById("validator")
-
-var password = document.getElementById("password")
-var toggler = document.getElementById("toggler")
-
-var specialChars = ["!","@","#","$","%","^","&","*"];
+var length = getById("length")
+var chars = getById("chars")
+var password = getById("password")
+var toggler = getById("toggler")
+var send = getById("send")
+var code = getById("code")
+var submit = getById("submit")
+var specialChars = ["!","@","#","$","%","^","&","*","(",")","_","-","+","|","<",">","[","]","{","}"];
 
 toggler.addEventListener("click", function (evt) {
     if(password.type === "password") {
@@ -16,10 +16,12 @@ toggler.addEventListener("click", function (evt) {
 })
 
 password.addEventListener("focus", function() {
-    if(validator.classList.contains("hide")) {
-        validator.classList.remove("hide")
-        validator.classList.add("show")
-    }
+        show("validator")
+        
+})
+
+password.addEventListener("blur", function() {
+        hide("validator")
 })
 
 password.addEventListener("input", function () {
@@ -30,11 +32,56 @@ password.addEventListener("input", function () {
     if(password.value.length < 8) {
         length.classList.remove("success")
     }
-
+    
     specialChars.forEach((char)=>{
-     if(values.includes(char)) {
-        chars.classList.add("success")
+        if(values.includes(char)) {
+            chars.classList.add("success")
      }
     })
+    
 })
 
+code.addEventListener("focus", function() {
+    show("hint")
+})
+code.addEventListener("blur", function() {
+    hide("hint")
+})
+code.addEventListener("input", function () {
+    if(code.value.length === 6){
+        submit.disabled = false 
+    }
+})
+
+send.addEventListener("click", function () {
+    send.disabled = true
+    let count = 59, timer = setInterval(() => {
+        send.textContent = `Resend Code: ${count-- - 1}s`
+        if(count == 0) {
+            clearInterval(timer)
+            send.textContent = `Resend Code`
+            send.disabled = false
+        }
+    }, 1000);
+})
+
+
+function getById(id) {
+return document.getElementById(id)
+}
+
+function hide(elemId) {
+    let elem = getById(elemId)
+    if(elem.classList.contains("show")) {
+        elem.classList.remove("show")
+        elem.classList.add("hide")
+    }
+}
+
+function show(elemId) {
+    let elem = getById(elemId)
+    if(elem.classList.contains("hide")) {
+        elem.classList.remove("hide")
+        elem.classList.add("show")
+    }
+}
