ca06dc55e766bc25b576828fa7459bb88730c6a0
Tim T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-08-29 19:29:14 +0000

    added link for terms

--- routes/auth/login.routes.js
+++ routes/auth/login.routes.js
@@ -1,4 +1,6 @@
 var express = require("express");
+const sqlite = require("../../sqlite");
+var bcrypt = require("bcrypt");
 var router = express.Router();
 
 router.get("/", function (req, res, next) {
@@ -7,6 +9,14 @@ router.get("/", function (req, res, next) {
 
 router.post("/", function (req, res, next) {
   let {email,password} = req.body
+  sqlite.all("SELECT * FROM users WHERE contact=?", [email], (err, rows) => {
+    console.log(rows)
+     if(bcrypt.compareSync(password, rows[0].password)){
+      res.redirect("/dash")
+     } else {
+      console.error("Incorrect password or username.")
+     }
+  })
 });
 
 
---
+++ sqlite.js
@@ -0,0 +1,4 @@
+const sqlite3 = require("sqlite3").verbose();
+const sqlite = new sqlite3.Database("./bin/ychurch.db");
+
+module.exports = sqlite;
