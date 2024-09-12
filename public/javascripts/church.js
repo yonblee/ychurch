12c0ae2c4f36a8d09dcac523909ae2d90ac07d1f
Tim T. Joe <106974662+TimTJoe@users.noreply.github.com>
2024-09-12 13:43:02 +0000

    added nav buttons

--- public/javascripts/church.js
+++ public/javascripts/church.js
@@ -1,39 +1,39 @@
-// $(function () {
+$(function () {
 
-//     if ($("#one").is(":visible")) {
-//         $("#prev").hide()
-//     } 
+    if ($("#one").is(":visible")) {
+        $("#prev").hide()
+    } 
     
-//     $("#two").hide()
-//     $("#three").hide()
-//     $("#next").on("click", function () {
+    $("#two").hide()
+    $("#three").hide()
+    $("#next").on("click", function () {
 
-//         $("#prev").show()
+        $("#prev").show()
 
-//         if ($("#one").is(":visible")) {
-//         $("#one").hide()
-//         $("#two").slideDown()
+        if ($("#one").is(":visible")) {
+        $("#one").hide()
+        $("#two").slideDown()
 
-//         } else if($("#two").is(":visible")) {
-//         $("#two").hide()
-//         $("#three").slideDown() 
-//         $("#next").hide()
-//         }
+        } else if($("#two").is(":visible")) {
+        $("#two").hide()
+        $("#three").slideDown() 
+        $("#next").hide()
+        }
 
-//     });
+    });
 
-//     $("#prev").on("click", function () {
-//         if ($("#two").is(":visible")) {
-//         $("#two").hide()
-//         $("#one").show().slideDown()
-//         $("#next").show()
-//         $("#prev").hide()
+    $("#prev").on("click", function () {
+        if ($("#two").is(":visible")) {
+        $("#two").hide()
+        $("#one").show().slideDown()
+        $("#next").show()
+        $("#prev").hide()
 
-//         } else if($("#three").is(":visible")) {
-//         $("#three").hide()
-//         $("#two").show().slideDown() 
-//         $("#next").show()
-//         }
-//     });
+        } else if($("#three").is(":visible")) {
+        $("#three").hide()
+        $("#two").show().slideDown() 
+        $("#next").show()
+        }
+    });
 
-// })
+})
